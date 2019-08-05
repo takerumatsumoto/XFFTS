20a697bf1850b123f2751aba766552f63dff7b13
takerumatsumoto <tatuyuki.takashima@gmail.com>
2019-08-05 09:48:52 +0000

    fix

--- initialize.py
+++ initialize.py
@@ -3,11 +3,12 @@
 import xfftspy
 
 # initialize XFFTS boards
->>> cmd = xfftspy.udp_client(host='localhost')
->>> cmd.stop()
->>> cmd.set_synctime(100000)          # synctime : 100 ms
->>> cmd.set_usedsections([1])         # use board : 1
->>> cmd.set_board_bandwidth(1, 200)  # bandwidth : 200 MHz
->>> cmd.configure()                   # apply settings
->>> cmd.caladc()                      # calibrate ADCs
->>> cmd.start()                       # start measurement
+cmd = xfftspy.udp_client(host='localhost')
+cmd.stop()
+cmd.set_synctime(100000)          # synctime : 100 ms
+cmd.set_usedsections([1,1])       # use board : 1
+cmd.set_board_bandwidth(1, 2000)  # bandwidth : 2000 MHz
+cmd.set_board_bandwidth(2, 2000)
+cmd.configure()                   # apply settings
+cmd.caladc()                      # calibrate ADCs
+cmd.start()                       # start measurement
